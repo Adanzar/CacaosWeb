202e8c2c58b3bb212c585cd01ea469f1bede0f01
Adan <chema6822@gmail.com>
2021-06-08 12:44:43 +0200

    Add store file

---
+++ cacaosWeb/frontend/src/redux/store/index.js
@@ -0,0 +1,11 @@
+import { applyMiddleware, compose, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from '../reducers/rootReducer';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export default (initialState) => createStore(
+  rootReducer,
+  initialState,
+  composeEnhancers(applyMiddleware(thunk))
+);
