e63f548f391bb110ea636e248415c8c1f762540e
Adan <chema6822@gmail.com>
2021-06-10 11:59:14 +0200

    Add main image to home -Fix problems in header

--- cacaosWeb/frontend/src/components/Details/details.jsx
+++ cacaosWeb/frontend/src/components/Details/details.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function Details() {
   return (
-    <h2>Soy Details</h2>
+    <img src="" alt="" />
   );
 }
--- cacaosWeb/frontend/src/components/Home/home.jsx
+++ cacaosWeb/frontend/src/components/Home/home.jsx
@@ -1,27 +1,30 @@
 /* eslint-disable react/jsx-curly-brace-presence */
 import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getProducts } from '../../redux/actions/actionCreators';
+import './home.scss';
 
 export default function Home() {
   const dispatch = useDispatch();
   const products = useSelector((store) => store.products);
+  //   const currentProduct = products.find((item) => !!item);
   useEffect(() => {
     dispatch(getProducts());
-  });
+  }, []);
 
   return (
-    <ul className="List__container">
-      {products.map((item) => (
-        <li>
-          <small>
-            {item.name}
-            {''}
-            {item.price.amount}
-          </small>
-          <img src={item.imagesUrls[0]} alt="" />
-        </li>
-      ))}
-    </ul>
+    <>
+      <div className="list__container">
+        <img src={products[0]?.imagesUrls[0]} alt="PRODUCTS" />
+        <p>{products[0]?.description}</p>
+      </div>
+      <h2>This weeks selection</h2>
+
+    </>
+
   );
+
+  // <div className="list__container">
+  //   <img src={'https://trello-attachments.s3.amazonaws.com/60bf42b01e72b913d536a078/800x500/d261b471ed775bad23872968a8a3be43/screen-0.jpg'} alt="PRODUCTS" />
+  // </div>
 }
