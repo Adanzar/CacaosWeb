1863aa9509f2db1464c8a3d21120ac133b794e72
Adan <chema6822@gmail.com>
2021-06-15 12:50:00 +0200

    Add bootstrap to detail component -Update cart component

--- cacaosWeb/frontend/src/components/Cart/cart.jsx
+++ cacaosWeb/frontend/src/components/Cart/cart.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { loadCart } from '../../redux/actions/cartActionCreators';
 
 export default function Cart() {
+  const dispatch = useDispatch();
+  const cart = useSelector((store) => store.cart);
+  useEffect(() => {
+    dispatch(loadCart());
+  });
   return (
-    <h2>Soy Cart</h2>
+    <>
+      <h4> Products in the cart </h4>
+      <div className="cart">
+        <ul className="cart__list">
+          {cart?.map((product) => (
+            <li key={product?._id} className="cart__list--products">
+              <div>
+                {product?.name}
+                {product?.price}
+              </div>
+            </li>
+          )) }
+        </ul>
+      </div>
+    </>
   );
 }
