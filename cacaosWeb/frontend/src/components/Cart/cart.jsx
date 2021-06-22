f58f0fd58016aeae14cffb8d37fd943734e9e1ef
Adan <chema6822@gmail.com>
2021-06-22 12:50:10 +0200

    Add funcionality to cart -Add fn to detail component -Update detail style

--- cacaosWeb/frontend/src/components/Cart/cart.jsx
+++ cacaosWeb/frontend/src/components/Cart/cart.jsx
@@ -1,15 +1,34 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { deleteInCart } from '../../redux/actions/cartActionCreators';
+import { modifyStock } from '../../redux/actions/actionCreators';
 
 export default function Cart() {
   const cart = useSelector((store) => store.cart);
+  const reduxProduct = useSelector((store) => store.product);
+  const [currentStock, setCurrentStock] = useState(reduxProduct?.stock);
+  const dispatch = useDispatch();
+  let changeStock = null;
+
+  function addStock() {
+    changeStock = currentStock + 1;
+    return changeStock;
+  }
+  function removeFromCart(index) {
+    addStock();
+    setCurrentStock(changeStock);
+    dispatch(modifyStock(reduxProduct._id, { stock: currentStock }));
+    dispatch(deleteInCart(index, cart));
+  }
+
+  console.log(cart);
 
   return (
     <>
       <h4> Products in the cart </h4>
       <div className="cart">
         <ul className="cart__list">
-          {cart?.map((product) => (
+          {cart?.map((product, index) => (
             <li key={product?._id} className="cart__list--products">
               <div>
                 {product?.name}
@@ -21,7 +40,14 @@ export default function Cart() {
               <p>
                 {product?.stock}
               </p>
+              <button
+                type="button"
+                onClick={() => { removeFromCart(index); }}
+              >
+                poner basurita
+              </button>
             </li>
+
           )) }
         </ul>
       </div>
--- cacaosWeb/frontend/src/redux/actions/cartActionCreators.js
+++ cacaosWeb/frontend/src/redux/actions/cartActionCreators.js
@@ -1,10 +1,20 @@
-/* eslint-disable no-debugger */
 import actionTypes from './actionTypes';
 
-export default function addToCart(product, cart) {
+export function addToCart(product, cart) {
   const newCart = [...cart, product];
   return {
     type: actionTypes.ADD_TO_CART,
     newCart
+  };
+}
+
+export function deleteInCart(index, cart) {
+  const newCart = [...cart];
+  newCart.splice(index, 1);
+  return (dispatch) => {
+    dispatch({
+      type: actionTypes.DELETE_FROM_CART,
+      newCart
+    });
   };
 }
